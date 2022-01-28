62be597119d470406623d753c64af4e0dc25b604
Ruben Lopes <ru4en@pm.me>
2022-01-28 14:04:34 +0000

    particles

--- console.js
+++ console.js
@@ -1,23 +1,16 @@
 
 
 var commandline = document.getElementById("console");
-var stringout = ["Hello, I am Ruben!", "NICE!"];
+var stringout = "Hello, I am Ruben!";
 var i = 0;
-var j = 0;
 
 function typing() {
-    if (i < (stringout[j].length)) {
-        commandline.innerHTML += stringout[j][i];
+    if (i < (stringout.length)) {
+        commandline.innerHTML += stringout[i];
+        console.log("ds");
         i++;
         setTimeout(typing, Math.floor(Math.random() * 700));
     }
 }
 
-function typer(){
-    j+=1;
-    i = 0;
-    typing();
-}
 typing();
-
-commandline.onclick = function(){typer()};
