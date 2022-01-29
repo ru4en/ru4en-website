ae97d274b21719e46097638d7adf367f691f68d0
Ruben Lopes <ru4en@pm.me>
2022-01-29 08:41:38 +0000

    added gradient to project backgroud

--- projects.js
+++ projects.js
@@ -29,14 +29,14 @@ readTextFile("projects.json", function(text){
     for(let i = 0; i < times; i++){
         console.log(obj[i].live_URL);
         if (obj[i].live_URL == "#"){            
-            var live_URL = `<div class="pr-1 w-50"><a href="" class="w-100 disabled btn btn-secondary">Unavailable</a></div>`
+            var live_URL = `<div class="pr-1 w-50"><a href="" class="w-100 disabled btn btn-secondary text-nowrap">Unavailable</a></div>`
         }
         else{
             var live_URL = `<div class="pr-1 w-50"><a href="${obj[i].live_URL}" class="w-100 btn btn-success">Live</a></div>`
         }
 
         if (obj[i].source_code == "#"){            
-            var source_code = `<div class="pr-1 w-50"><a href="" class="w-100 disabled btn btn-secondary">Unavailable</a></div>`
+            var source_code = `<div class="pr-1 w-50"><a href="" class="w-100 disabled btn btn-secondary text-nowrap">Unavailable</a></div>`
         }
         else{
             var source_code = `<div class="pr-1 w-50"><a href="${obj[i].source_code}" class="w-100 col-auto btn btn-success">Github</a></div>`
