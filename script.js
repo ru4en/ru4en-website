6fedb14bcd639ffb57d4faf54064e0234c8ce72c
Ruben Lopes <ru4en@pm.me>
2022-04-14 00:15:50 +0100

    Improved form

--- script.js
+++ script.js
@@ -5,26 +5,30 @@ $(function(){
 
 function notification(msg, btncl) {
     var contfb = document.createElement('div');
-    contfb.innerHTML = '<div onclick="this.remove()" class="btn btn-primary alert alert-dismissible fade show '+ btncl +' m-3" role="alert" style="position: fixed; top: 60px;"> ' + msg + ' <button type="button" class="btn-close" aria-label="Close"></button> </div>';
+    contfb.innerHTML = '<div onclick="this.remove()" class="btn alert alert-dismissible '+ btncl +' m-3" role="alert" style="position: fixed; top: 60px;"> ' + msg + ' <button type="button" class="btn-close" aria-label="Close"></button> </div>';
     main.appendChild(contfb);
 }
 
 function contFormSubmited(contForm) { 
 
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let msg = document.getElementById('msg').value;
-    let main = document.getElementById('main');
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let msg = document.getElementById('msg');
+
+    if(name.value == "" || email.value == "" || msg.value == "") {
+        notification("Unsucessful: Please Make sure all fields are valid", "btn-danger");
+        return;
+    }
 
     Pageclip.send('hkKqTgb3JqRLL16rUVVGGAQIBBx1ZWku', 'PortfolioContactMe', {
-        name: name,
-        email: email,
-        msg: msg
+        name: name.value,
+        email: email.value,
+        msg: msg.value
     }
     )
 
-    name = "";
-    email = "";
-    msg = "";
-    notification("Sucessful: Thanks "+ name +"! I will make sure to respond you.", "btn-success");
+    notification("Sucessful: Thanks "+ name.value +"! I will make sure to respond you.", "btn-success");
+    name.value = null;
+    email.value = null;
+    msg.value = null;
 }
